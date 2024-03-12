dca93cb250177ff994e4c22ff3d2d29e551365d1
biofelip <felipe.1092@gmail.com>
2024-03-12 16:30:30 +0100

    picture merger changes

--- picture_merger.py
+++ picture_merger.py
@@ -1,17 +1,26 @@
-from PIL import Image
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 import glob
 import os
 
-root_path="E:\\drone footage\\16-11-23\\drone\\DCIM\\100MEDIA"
+root_path="E:\\drone footage\\16-11-23\\drone"
 images=glob.glob(os.path.join(root_path, "**.jpg"))
 
 # variables to include
 hor_overlap=0.4
 ver_overlap=0.4
+resize_factor=0.4
+
 
 
 im1=Image.open(images[0])
 im2=Image.open(images[1])
 
-im_combiend=Image.new('RGB', (im1.width+im2.width, im1.height))
+im1_r=im1.resize([int(resize_factor *s) for s in im1.size])
+im2_r=im2.resize([int(resize_factor *s) for s in im2.size])
+
+im1_r.save("im1_r.jpg")
+im2_r.save("im2_r.jpg")
+# vertical merging the image has width of im1 and im2 but 0.8* height of im1+im2
+im_combiend=Image.new('RGB', (im1_r.width, int(0.8*(2*im1_r.height))))
 im_combiend.show()
